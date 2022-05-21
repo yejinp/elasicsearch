58af983bc2f482407c171a5696f279b897bf3de5
yejinp <ye895937@126.com>
2022-05-21 18:03:45 +0800

    add shutdown.sh

---
+++ chapter1/shutdown.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="${ES_HOST}:${ES_PORT}/_shutdown?pretty"
+
+curl -H "Content-Type: application/json" -XPUT ${url} 
+
