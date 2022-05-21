40ce162af2c057925d910f91e6e260207425fc12
yejinp <ye895937@126.com>
2022-05-21 19:33:15 +0800

    add files

---
+++ chapter3/put_website_blog_123.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="${ES_HOST}:${ES_PORT}/website/blog/123?pretty"
+
+curl -H "Content-Type: application/json" -XPUT ${url}  -d '
+{
+    "title": "My first blog entry",
+    "text": "Just trying this out...",
+    "date": "2014/01/01"
+}
+'
