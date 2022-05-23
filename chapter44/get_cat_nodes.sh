2187f8ac308beca103d579e4f87b31c4d0079c57
yejinp <ye895937@126.com>
2022-05-23 23:30:45 +0800

    add get_cat_nodes.sh

---
+++ chapter44/get_cat_nodes.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+header="Content-Type: application/json"
+url="${ES_HOST}:${ES_PORT}/_cat/nodes?v"
+
+curl -s -H "${header}"  -XGET ${url}  
