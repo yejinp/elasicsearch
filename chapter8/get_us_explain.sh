708a7a7816489de9af9d4e99ed557c8ff72b3028
yejinp <ye895937@126.com>
2022-05-23 22:45:32 +0800

    fix script

--- chapter8/get_us_explain.sh
+++ chapter8/get_us_explain.sh
@@ -5,7 +5,7 @@ source ${home}/es.env
 [ -z ${ES_HOST} ]  && ES_HOST="localhost"
 [ -z ${ES_PORT} ]  && ES_PORT="9200"
 
-url="http://${ES_HOST}:${ES_PORT}/us/tweet/_search?explain&pretty"
+url="http://${ES_HOST}:${ES_PORT}/us/tweet/12/_explain?pretty"
 header="Content-Type: application/json"
 
 curl -H "${header}" -XGET ${url}  -d '
