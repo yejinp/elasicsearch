a692529e1dad41d72468e381324d7ba4a4b6ac57
yejinp <ye895937@126.com>
2022-05-23 23:04:33 +0800

    add  blocking_for_statusus_change.sh

---
+++ chapter44/blocking_for_statusus_change.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+header="Content-Type: application/json"
+url="${ES_HOST}:${ES_PORT}/_cluster/health?wait_for_status=green&pretty"
+
+curl -H "${header}"  -XGET ${url}  
