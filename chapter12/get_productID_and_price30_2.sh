993969f69d14386610923b2f5f4f0a84e518a4d2
yejinp <ye895937@126.com>
2022-05-24 22:33:18 +0800

    fix

--- chapter12/get_productID_and_price30_2.sh
+++ chapter12/get_productID_and_price30_2.sh
@@ -1,4 +1,14 @@
-curl -H "Content-Type: application/json" -XPOST 'localhost:9220/my_store/products/_search?pretty' -d '
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="${ES_HOST}:${ES_PORT}/my_store/products/_search?pretty"
+header="Content-Type: application/json"
+
+curl -H "${header}" -XPOST ${url} -d '
 {
 	"query" : {
 	      "bool":{
