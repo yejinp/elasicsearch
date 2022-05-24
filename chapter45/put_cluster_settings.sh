1e9648874db0c55573dd7a8ddf73a0a880414472
yejinp <ye895937@126.com>
2022-05-24 08:34:09 +0800

    fix url

--- chapter45/put_cluster_settings.sh
+++ chapter45/put_cluster_settings.sh
@@ -1,4 +1,14 @@
-curl -H "Content-Type: application/json" -XPUT 'localhost:9220/_cluster/settings?pretty' -d '
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="${ES_HOST}:${ES_PORT}/_cluster/settings?pretty"
+header="Content-Type: application/json"
+
+curl -H "${header}" -XGET ${url}   -d '
 {
 	"persistent" : {
 	 	"discovery.zen.minimum_master_nodes" : 2
