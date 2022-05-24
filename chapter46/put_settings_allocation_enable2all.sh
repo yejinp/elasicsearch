0d3a52fa16b18cf7b537a809bdb64b275f76ac8b
yejinp <ye895937@126.com>
2022-05-24 19:59:05 +0800

    add put_settings_allocation_enable2all.sh

---
+++ chapter46/put_settings_allocation_enable2all.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+header="Content-Type: application/json"
+url="${ES_HOST}:${ES_PORT}/_cluster/settings?pretty"
+
+curl -H "${header}" -XGET ${url}  -d '
+{
+	"transient" : {
+		"cluster.routing.allocation.enable" : "all"
+	}
+}
+'
