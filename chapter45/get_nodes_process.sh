3dc0eb242777dedec87b7945b3d2789917fb7a6f
yejinp <ye895937@126.com>
2022-05-24 08:32:39 +0800

    fix url

--- chapter45/get_nodes_process.sh
+++ chapter45/get_nodes_process.sh
@@ -1,1 +1,11 @@
-curl -H "Content-Type: application/json" -XGET 'localhost:9220/_nodes/process?pretty' 
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="${ES_HOST}:${ES_PORT}/_nodes/process?pretty"
+header="Content-Type: application/json"
+
+curl -H "${header}" -XGET ${url}  
