f2852e8f99eafc6bcb8349b3cafccdb9583a1369
yejinp <ye895937@126.com>
2022-05-24 22:34:05 +0800

    fix

--- chapter12/post_exact_bulk.sh
+++ chapter12/post_exact_bulk.sh
@@ -1,4 +1,14 @@
-curl -H "Content-Type: application/json" -XPOST 'localhost:9220/my_index/my_type/_bulk' -d '
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="${ES_HOST}:${ES_PORT}/my_index/my_type/_bulk?pretty"
+header="Content-Type: application/json"
+
+curl -H "${header}" -XPOST ${url} -d '
 { "index": { "_id": 1 }}
 { "tags" : ["search"], "tag_count" : 1 }
 { "index": { "_id": 2 }}
