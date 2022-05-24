b0e21ed2afcb79bc3eb1a5e8b83b3eb3b3c6f5d9
yejinp <ye895937@126.com>
2022-05-24 19:55:01 +0800

    add put_settings_allocation_enable.sh

--- chapter46/put_settings_allocation_enable.sh
+++ chapter46/put_settings_allocation_enable.sh
@@ -1,4 +1,14 @@
-curl -H "Content-Type: application/json" -XPUT 'localhost:9200/_cluster/settings?pretty'  -d '
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+header="Content-Type: application/json"
+url="${ES_HOST}:${ES_PORT}/_cluster/settings?pretty"
+
+curl -H "${header}" -XGET ${url}  -d '
 {
 	"transient" : {
 		"cluster.routing.allocation.enable" : "none"
