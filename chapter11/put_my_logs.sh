0aa967a998d2d2f42b7f86058be9b4489e2a40ca
yejinp <ye895937@126.com>
2022-05-24 22:49:47 +0800

    add put_my_logs.sh

---
+++ chapter11/put_my_logs.sh
@@ -0,0 +1,17 @@
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="${ES_HOST}:${ES_PORT}/my_logs?pretty"
+header="Content-Type: application/json"
+
+curl -H "${header}" -XPUT ${url}  -d '
+    {
+         "settings": {
+                "refresh_interval": "30s"
+           }
+   }
+'
