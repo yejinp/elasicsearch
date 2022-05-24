272aed911dc4ecc9914a441507c6ce1b19fc986a
yejinp <ye895937@126.com>
2022-05-24 22:41:57 +0800

    add put_my_logs_settings_30s.sh

---
+++ chapter11/put_my_logs_settings_30s.sh
@@ -0,0 +1,15 @@
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="${ES_HOST}:${ES_PORT}/my_logs/_settings?pretty"
+header="Content-Type: application/json"
+
+curl -H "${header}" -XPUT ${url}  -d '
+{
+     "refresh_interval": "30s"
+}
+'
