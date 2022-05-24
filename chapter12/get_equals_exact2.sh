31ae98041ea7137d752a9c8fd7bf1983cd977e46
yejinp <ye895937@126.com>
2022-05-24 22:31:44 +0800

    fix

--- chapter12/get_equals_exact2.sh
+++ chapter12/get_equals_exact2.sh
@@ -1,4 +1,14 @@
-curl -H "Content-Type: application/json" -XPOST 'localhost:9220/my_index/my_type/_search?pretty' -d '
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="${ES_HOST}:${ES_PORT}/my_index/my_type/_search?pretty"
+header="Content-Type: application/json"
+
+curl -H "${header}" -XPOST ${url} -d '
 {
 	"query" : {
 		"bool": {
