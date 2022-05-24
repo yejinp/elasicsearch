48d56a4f15f2326b149968fccbbf9ff38e7d5152
yejinp <ye895937@126.com>
2022-05-24 19:52:50 +0800

    add put_settings_cluster_slowlog.sh put_settings_index_slowlog.sh

--- chapter46/put_settings_index_slowlog.sh
+++ chapter46/put_settings_index_slowlog.sh
@@ -1,4 +1,14 @@
-curl -H "Content-Type: application/json" -XPUT 'localhost:9200/my_index/_settings?pretty'  -d '
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+header="Content-Type: application/json"
+url="${ES_HOST}:${ES_PORT}/my_index/_settings?pretty"
+
+curl -H "${header}" -XGET ${url}  -d '
 {
 	"index.search.slowlog.threshold.query.warn" : "10s",
 	"index.search.slowlog.threshold.fetch.debug": "500ms",
