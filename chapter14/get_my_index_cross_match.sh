e93be5e65d1e24ef3421686bd223bb7f5a9f3b22
yejinp <ye895937@126.com>
2022-05-25 19:45:49 +0800

    add files

---
+++ chapter14/get_my_index_cross_match.sh
@@ -0,0 +1,21 @@
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="${ES_HOST}:${ES_PORT}/my_index/_search?pretty"
+header="Content-Type: application/json"
+
+curl -H "${header}" -XGET ${url} -d '
+{
+	"query" : {
+		"multi_match": {
+			"query": "jumping rabbits",
+ 			"type": "most_fields",
+ 			"fields": [ "title", "title.std" ]
+		}
+	}
+}
+'
---
+++ chapter14/put_my_index_settings.sh
@@ -0,0 +1,32 @@
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="${ES_HOST}:${ES_PORT}/my_index?pretty"
+header="Content-Type: application/json"
+
+curl -H "${header}" -XPUT ${url} -d '
+{
+	"settings": { "number_of_shards": 1 },
+	"mappings": {
+		"my_type": {
+			"properties": {
+				"title": {
+					"type": "text",
+					"analyzer": "english",
+					"fields": {
+						"std": {
+							"type": "text",
+							"analyzer": "standard"
+						}
+ 					}
+ 				}
+ 			}
+ 		}
+ 	}
+}
+
+'
