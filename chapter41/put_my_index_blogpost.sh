963cad9f4614933d44e2659ccab09e989d1fda1d
yejinp <ye895937@126.com>
2022-05-25 23:01:39 +0800

    add files

---
+++ chapter41/put_my_index_blogpost.sh
@@ -0,0 +1,33 @@
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="${ES_HOST}:${ES_PORT}/my_index/blogpost/1?pretty"
+header="Content-Type: application/json"
+
+curl -H "${header}" -XPUT ${url} -d '
+{ 
+	"title": "Nest eggs",
+	"body": "Making your money work...",
+	"tags": [ "cash", "shares" ],
+	"comments": [
+	{
+		"name": "John Smith",
+		"comment": "Great article",
+		"age": 28,
+		"stars": 4,
+		"date": "2014-09-01"
+	},
+	{
+		"name": "Alice White",
+		"comment": "More like this please",
+		"age": 31,
+		"stars": 5,
+		"date": "2014-10-22"
+	}
+	]
+}
+'
