7e80011fd13840648b56ae47ef517dc11bb2c20b
yejinp <ye895937@126.com>
2022-05-27 19:45:00 +0800

    fix

--- chapter10/get_spanish_docs_analyze_2.sh
+++ chapter10/get_spanish_docs_analyze_2.sh
@@ -1,4 +1,14 @@
-curl -H "Content-Type: application/json" -XPOST  --raw '192.168.136.237:9200/spanish_docs/_analyze?pretty'  -d '
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="http://${ES_HOST}:${ES_PORT}/spanish_docs/_analyze?pretty"
+header="Content-Type: application/json"
+
+curl -H "${header}" -XPOST ${url}  -d '
 {
 	"analyzer": "es_std",
 	"text" : "Is this déjà vu?"
