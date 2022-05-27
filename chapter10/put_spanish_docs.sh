627e3a1af4d401d25d1ea92a13fe427719cb808b
yejinp <ye895937@126.com>
2022-05-27 08:23:47 +0800

    add put_spanish_docs.sh

---
+++ chapter10/put_spanish_docs.sh
@@ -0,0 +1,24 @@
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="http://${ES_HOST}:${ES_PORT}/spanish_docs?pretty"
+header="Content-Type: application/json"
+
+curl -H "${header}" -XPUT ${url} -d '
+{
+	"settings": {
+	"analysis": {
+	"analyzer": {
+	"es_std": {
+	"type": "standard",
+	"stopwords": "_spanish_"
+}
+}
+}
+}
+}
+'
