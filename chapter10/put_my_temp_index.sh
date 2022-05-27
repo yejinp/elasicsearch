baf677999695f8ec68466d74fbebc130bba21935
yejinp <ye895937@126.com>
2022-05-27 08:19:29 +0800

    add put_my_temp_index.sh

---
+++ chapter10/put_my_temp_index.sh
@@ -0,0 +1,18 @@
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="http://${ES_HOST}:${ES_PORT}/my_temp_index?pretty"
+header="Content-Type: application/json"
+
+curl -H "${header}" -XPUT ${url} -d '
+{
+	"settings": {
+	"number_of_shards" : 1,
+	"number_of_replicas" : 0
+}
+}
+'
