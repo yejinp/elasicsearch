6f4d6f9bc5613340894731800763c78269e189b3
yejinp <ye895937@126.com>
2022-05-28 18:24:36 +0800

    add files

---
+++ chapter30/get_cars_transactions_post_filter.sh
@@ -0,0 +1,31 @@
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="${ES_HOST}:${ES_PORT}/cars/transactions/_search?pretty"
+header="Content-Type: application/json"
+
+curl -H "${header}" -XGET ${url} -d '
+{
+	"query": {
+		"match": {
+			"make": "ford"
+		}
+	},
+	"post_filter": {
+		"term": {
+			"color": "green"
+		}
+	},
+	"aggs": {
+		"all_colors": {
+			"terms": {
+				"field": "color"
+			}
+		}
+	}
+}
+'
