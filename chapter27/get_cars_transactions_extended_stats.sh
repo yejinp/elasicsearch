7c116ee803c08856352371f5e1edd0325d15f8bd
yejinp <ye895937@126.com>
2022-05-28 18:06:35 +0800

    add files

---
+++ chapter27/get_cars_transactions_extended_stats.sh
@@ -0,0 +1,30 @@
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="${ES_HOST}:${ES_PORT}/cars/transactions/_search?pretty"
+header="Content-Type: application/json"
+
+curl -H "${header}" -XGET ${url} -d '
+{
+	"size": 0,
+	"aggs": {
+		"makes": {
+			"terms": {
+				"field": "make",
+				"size": 10
+			},
+			"aggs": {
+				"stats": {
+					"extended_stats": {
+						"field": "price"
+					}
+				}
+			}
+		}
+	}
+}
+'
