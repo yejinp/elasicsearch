60ca2199ebc2ef2a2ae3be48cf9dc4e8380452b2
yejinp <ye895937@126.com>
2022-05-28 18:17:25 +0800

    add get_cars_transactions_terms_over_10000.sh

---
+++ chapter30/get_cars_transactions_terms_over_10000.sh
@@ -0,0 +1,26 @@
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="${ES_HOST}:${ES_PORT}/cars/transactions/_search?pretty"
+header="Content-Type: application/json"
+
+curl -H "${header}" -XGET ${url} -d '
+{
+	"query" : {
+		"range":{
+			"price":{"gte": 10000}
+		}
+	},
+	"aggs": {
+		"single_avg_price": {
+			"avg": {
+				"field": "price"
+			}
+		}
+	}
+}
+'
