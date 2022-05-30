b145ba4ea26d5ab55e80a5830f6cc3c4f546a0af
yejinp <ye895937@126.com>
2022-05-30 08:48:14 +0800

    add files

---
+++ chapter18/put_my_index_blog_1.sh
@@ -0,0 +1,16 @@
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="${ES_HOST}:${ES_PORT}/my_index/blog/1?pretty"
+header="Content-Type: application/json"
+
+curl -H "${header}" -XPUT ${url} -d '
+{
+	"title": "I\u0027m happy for this fox"
+}
+
+'
