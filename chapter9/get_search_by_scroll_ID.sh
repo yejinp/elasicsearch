6013b904eaeb3a5afa34dbb6dadebf6200768e69
yejinp <ye895937@126.com>
2022-06-01 20:00:17 +0800

    add files

---
+++ chapter9/get_search_by_scroll_ID.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="${ES_HOST}:${ES_PORT}/_search/scroll?scroll=1m&pretty"
+header="Content-Type: application/json"
+
+curl -H "${header}" -XGET ${url} -d 'c2Nhbjs1OzExODpRNV9aY1VyUVM4U0NMd2pjWlJ3YWlBOzExOTpRNV9aY1VyUVM4U0NMd2pjWlJ3YWlBOzExNjpRNV9aY1VyUVM4U0NMd2pjWlJ3YWlBOzExNzpRNV9aY1VyUVM4U0NMd2pjWlJ3YWlBOzEyMDpRNV9aY1VyUVM4U0NMd2pjWlJ3YWlBOzE7dG90YWxfaGl0czoxOw=='
