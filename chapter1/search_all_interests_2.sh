71eecd7dcbb8af0fefb2d8fc66f83284859c863b
yejinp <ye895937@126.com>
2022-06-02 22:21:09 +0800

    add file

--- chapter1/search_all_interests_2.sh
+++ chapter1/search_all_interests_2.sh
@@ -1,4 +1,12 @@
-curl -H "Content-Type: application/json" -XGET 'localhost:9200/megacorp/employee/_search?pretty'  -d '
+#!bin/bash
+
+home=$(dirname $0)
+source ${home}/es.env
+[ -z ${ES_HOST} ]  && ES_HOST="localhost"
+[ -z ${ES_PORT} ]  && ES_PORT="9200"
+
+url="${ES_HOST}:${ES_PORT}/megacorp/employee/_search?pretty"
+curl -H "Content-Type: application/json" -XGET ${url}  -d '
 {
 	"aggs": {
 		"all_interests": {
